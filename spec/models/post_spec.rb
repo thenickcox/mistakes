e137a0ea0407826580f263c222ff93ac7b13ddc4
thenickcox <nick@nickcox.me>
2013-10-16 20:57:28 -0700

    Add test for posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:nickname) { nil }
+  let(:attrs) { {
+    content: 'Content',
+    title: 'Title',
+    nickname: nickname,
+  } }
+
+  context 'honeypot not filled' do
+    it 'should create a valid post with minimal attrs' do
+      expect { Post.create!(attrs) }.not_to raise_error
+    end
+  end
+
+  context 'honeypot is filled' do
+    let(:nickname) { 'Pee-Wee' }
+    it 'should not create a post if there is a nickname (honeypot)' do
+      expect { Post.create!(attrs) }.to raise_error
+    end
+  end
+
+
 end
